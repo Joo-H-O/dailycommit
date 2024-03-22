461263491e4b14261abbb819b5cbdbf73bcb0ae6
H <160995978+Joo-H-O@users.noreply.github.com>
2024-03-22 13:39:06 +0900

    [level 0] Title: 카운트 업, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -0,0 +1,5 @@
+def solution(start_num, end_num):
+    answer = [] #make a List
+    for i in range(start_num, end_num + 1): #From Start number to end number
+        answer.append(i) #input answer to List
+    return answer
