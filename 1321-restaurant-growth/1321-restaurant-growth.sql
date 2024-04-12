ac0eece398dc51312cf0633d5fa8ee8991d72f04
H <160995978+Joo-H-O@users.noreply.github.com>
2024-04-12 17:27:28 +0900

    Time: 665 ms (65.76%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -4,14 +4,11 @@ SELECT visited_on,
     ROUND((amount / 7), 2) AS average_amount #rounded to two decimal places
 FROM (
     SELECT visited_on, 
-        CASE 
-            WHEN visited_on < DATE_ADD((SELECT MIN(visited_on) FROM Customer), INTERVAL 6 DAY) THEN NULL
-            #Only SUM when rows exist enough (current day + 6 days before = MIN(date) + 6 days after)
-            ELSE SUM(amount_per_date) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
-        END AS amount #Return SUM(amount) for each date
+        SUM(amount_per_date) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount #Return SUM(amount) for each date
     FROM (
         SELECT *, SUM(amount) AS amount_per_date
         FROM Customer
         GROUP BY visited_on) Sub) Sub2
-WHERE amount IS NOT NULL #Return result only from 2019-01-07
+WHERE visited_on >= DATE_ADD((SELECT MIN(visited_on) FROM Customer), INTERVAL 6 DAY)
+#Only SUM when rows exist enough (current day + 6 days before = MIN(date) + 6 days after)
 ORDER BY visited_on #ORDER BY condition
